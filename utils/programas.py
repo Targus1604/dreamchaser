aae2d4e58fb1d6d471e6f18933447b4b842f49f7
Carlos Ramírez <cramirezmun@unal.edu.co>
2025-01-28 17:45:02 -0500

    Añade programas de prueba

---
+++ utils/programas.py
@@ -0,0 +1,13 @@
+programaPrueba = """
+import "libreria.txt"
+const PI = 3.141592654
+
+const E = 2.718281828
+a = 0
+calcular = verdadero
+
+si calcular # Esto es un comentario
+    a = PI * a # Se va a eliminar
+sino a == 3
+    a = E
+"""
