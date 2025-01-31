9bd7274f7daba52c8a676207ac647becb94cd64f
JuMad-SE <jumadridc@unal.edu.co>
2025-01-31 17:45:06 -0500

    Actualiza el código de el enlazador cargador y lo hace más fiel con la tarea 16

--- enlazadorCargador.py
+++ enlazadorCargador.py
@@ -1,25 +1,32 @@
 # Enlazador-Cargador
-def linker_loader(object_code, base_address=0x1000):
-    memory_map = {}
-    address = base_address
-    executable_code = []
+def enlazador_cargador(codigo_entrada, direccion_base=0x00000):
+    TAMANO_MEMORIA = 2048  # Tamaño de la memoria en celdas
+    mapa_memoria = {}
+    direccion = direccion_base
+    codigo_ejecutable = []
 
-    for instr in object_code:
-        memory_map[address] = instr
-        executable_code.append(f"{address:04X}: {instr}")
-        address += 1
+    for instr in codigo_entrada:
+        if direccion >= TAMANO_MEMORIA:
+            raise MemoryError("Se ha excedido el tamaño de la memoria.")
+        mapa_memoria[direccion] = instr
+        codigo_ejecutable.append(f"{direccion:04X}: {instr}")
+        direccion += 1
 
-    return executable_code, memory_map
+    return codigo_ejecutable, mapa_memoria
 
 
-# Entrada código máquina
-object_code = ["0001 0001 0010", "0010 0001 0010"]  # ADD R1, R2  # STORE R1, R2
+# Entrada código máquina (32 bits por instrucción)
+codigo_entrada = [
+    "00000001000100100000000000000000",  # ADD R1, R2
+    "00000010000100100000000000000000",  # STORE R1, R2
+    "00000010000100100000000000000000",  # STORE R1, R2
+]
 
 # Ejecutar enlazador-cargador
-exe_code, mem_map = linker_loader(object_code)
+codigo_ejecutar, mapa_memoria = enlazador_cargador(codigo_entrada)
 
 # Salida del enlazador-cargador
-for line in exe_code:
+for line in codigo_ejecutar:
     print(line)
 
-print(mem_map)
+print("Memoria", mapa_memoria)
