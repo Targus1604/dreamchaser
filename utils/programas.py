153ac05a490e9740ef8aa47a089a3f1a5aef5d65
Carlos Ramírez <cramirezmun@unal.edu.co>
2025-01-31 18:54:52 -0500

    Añade extensiones regulares y mejora reconocimiento de tokens

--- utils/programas.py
+++ utils/programas.py
@@ -1,9 +1,9 @@
 programaPrueba = """
-import "libreria.txt"
+importar 'libreria.txt'
 const PI = 3.141592654
 
 const E = 2.718281828
-a = 0
+a_2 = 2.41e-2
 calcular = verdadero
 
 si calcular # Esto es un comentario
