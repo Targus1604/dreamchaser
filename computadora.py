04c5e6ed12e73ff913caf0f9f003fbd4b62c8012
JuMad-SE <jumadridc@unal.edu.co>
2025-01-31 19:03:09 -0500

    Añade reconocimiento de errores en el caso en que el código cargado supere la memoria o directamente no se pase

--- computadora.py
+++ computadora.py
@@ -13,6 +13,7 @@ class Computadora:
     def __init__(self, tamano_memoria=2048):
         # Simula la memoria (cada celda de memoria almacena un entero de 32 bits)
         self.memoria = [0] * tamano_memoria
+        self.registros = [0] * 8
 
         """---------------------------------------------------------------------------------
         BANDERAS DE DREAMCHASER
@@ -30,27 +31,44 @@ class Computadora:
         # Puntero de instrucción (PC)
         self.pc = 0
 
+    """
+    -------------------------
+    ENLAZADOR - CARGADOR
+    -------------------------
+    """
+
     # Carga el código binario proporcionado por el ensamblador de manera contigua en memoria
     # Comenzando desde el 0
-    def cargar_codigo(self, codigo_entrada, direccion_base=0x0000):
-        mapa_memoria = enlazador_cargador(codigo_entrada, direccion_base)
+    def cargar_codigo(self, codigo_entrada, direccion_de_inicio=0x0000):
+        mapa_memoria = enlazador_cargador(codigo_entrada, direccion_de_inicio)
         for direccion, instr in mapa_memoria.items():
             self.memoria[direccion] = instr
 
     # Muestra únicamente las celdas de memoria con instrucciones (usar self.memoria para un paneo completo)
     def mostrar_memoria(self):
+        memoria_ocupada = 0
         for direccion, instr in enumerate(self.memoria):
             if instr != 0:
+                memoria_ocupada += 1
+                # La dirección es mostrada en exadecimal
                 print(f"{direccion:04X}: {instr}")
+        if memoria_ocupada == 0:
+            print("No hay instrucciones en memoria")
 
 
 # Entrada código máquina (32 bits por instrucción)
 codigo_entrada = [
-    "00000001000100100000000000000000",  # ADD R1, R2
-    "00000010000100100000000000000000",  # STORE R1, R2
-    "00000010000100100000000000000000",  # STORE R1, R2
+    "00000000000000010100000000100000",
+    "00000000000000010100100000000000",
+    "00000000000000010101000000000001",
+    "00000000000000000000100000000111",
+    "00000000000000000000000101001000",
+    "00000000000000000000000110000010",
+    "00000000000000000011100000000011",
+    "00000000000000011000100000001001",
+    "00000000000000000000000000000000",
 ]
 
 dreamChaser = Computadora()
-dreamChaser.cargar_codigo(codigo_entrada)
+dreamChaser.cargar_codigo(codigo_entrada, direccion_de_inicio=16)
 dreamChaser.mostrar_memoria()
--- enlazadorCargador.py
+++ enlazadorCargador.py
@@ -1,6 +1,8 @@
 """
 Función que carga el código máquina retornado
 por el ensamblador en memoria para ser ejecutado
+resolviendo las direcciones en los casos en que 
+sea necesario
 """
 
 
@@ -9,10 +11,15 @@ def enlazador_cargador(codigo_entrada, direccion_base=0x00000):
     TAMANO_MEMORIA = 2048  # Tamaño de la memoria en celdas
     mapa_memoria = {}
     direccion = direccion_base
+    if len(codigo_entrada) == 0:
+        raise ValueError("El código de entrada no puede tener una longitud de 0.")
 
     for instr in codigo_entrada:
         if direccion >= TAMANO_MEMORIA:
-            raise MemoryError("Se ha excedido el tamaño de la memoria.")
+            raise MemoryError(
+                "Se ha excedido el tamaño de la memoria para la siguiente instrucción",
+                instr,
+            )
         mapa_memoria[direccion] = instr
         direccion += 1
 
